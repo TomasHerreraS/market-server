f13a01441640d266a20608a1c064b42ad5a8c593
Tomás <tomas.edhs@gmail.com>
2023-11-30 14:29:01 -0300

    user controller variables, express json

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -4,9 +4,9 @@ import jwt from 'jsonwebtoken';
 import pool from '../db/db';
 
 export const createUser = async(req: Request, res: Response) => {
-  const { name, lastname, rol, email, password, phone, address } = req.body;
-  const response = await pool.query('INSERT INTO users ("name", "lastname", "rol", "email", "password", "phone", "address") VALUES ($1, $2, $3, $4, $5, $6, $7)',
-  [name, lastname, rol, email, Md5.hashStr(password), phone, address])
-  res.json({name, lastname, email, phone, address})
+  const { name, lastname, rol_id, email, password, phone, state, city, address } = req.body;
+  const response = await pool.query('INSERT INTO users ("name", "lastname", "rol_id", "email", "password", "phone", "state", "city", "address") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+  [name, lastname, rol_id, email, Md5.hashStr(password), phone, state, city, address])
+  res.json({name, lastname, email, phone, state, city, address})
   return response;
 }
--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,11 @@ import { addUser } from './routes/user.route';
 
 const app = express();
 
+app.use(express.json()); // Permite reconocer y analizar solicitudes en formato JSON
 // Middleware para habilitar CORS
-app.use(cors({
-  origin: 'http://localhost:3001'
-}));
+app.use(cors());
 
-// Ruta de ejemplo
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
-});
+//routes
+app.use(addUser);
 
-const PORT = 3001; // Puerto en el que se ejecutará el servidor
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+app.listen(3001);
